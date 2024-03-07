35c4927f5c375faa0a5f6f895d969414b4c8af3b
alicialf12 <alicia.asokawati@yahoo.com>
2024-03-07 09:47:33 +0800

    e. Layout dengan Column

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
   
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,26 +15,28 @@ class MyApp extends StatelessWidget{
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Praktikum Layout Flutter'),
+        title: const Text('Praktikum Layout Flutter'),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
           Container(color: Colors.red, height: 100, width: 100),
-          Container(color: Colors.orange, height: 50, width: 100),
-          Container(color: Colors.yellow, height: 80, width: 100),
+          Container(color: Colors.orange, height: 50, width: 50),
+          Container(color: Colors.yellow, height: 80, width: 80),
         ],
       ),
     );
